fc5d2c185d3d7ba4f4e311d01d42c2545dd8560c
Yan Yan <evitayan@google.com>
2023-04-12 22:56:19 +0000

    breeze: Add feature flag for IPSEC_TUNNEL_MIGRATION
    This commit adds the IPSEC_TUNNEL_MIGRATION feature flag,
    with XFRM_MIGRATE used as the implementation
    
    Bug: 273379472
    Test: build, flash and verify with "adb shell pm list features"
    Test: atest IpSecManagerTunnelTest
    Change-Id: Id2ee3e65183e32ea63f6251ba921e1e4c6b09ed0

--- device.mk
+++ device.mk
@@ -260,7 +260,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+# Network
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # Overlays
