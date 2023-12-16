bc721fb8ee5c5a0f1a559418e707c1517b80185d
Giovanni Ricca <giovanniricca@proton.me>
2023-12-16 11:52:08 +0100

    breeze: Simplify 64bit audio service soong flag
    Change-Id: I15d02bc5c9d7fadf4d0dc8abc448710c5fb63a37

--- device.mk
+++ device.mk
@@ -53,9 +53,7 @@ PRODUCT_PACKAGES += \
     otapreopt_script
 
 # Audio
-SOONG_CONFIG_NAMESPACES += android_hardware_audio
-SOONG_CONFIG_android_hardware_audio += run_64bit
-SOONG_CONFIG_android_hardware_audio_run_64bit := true
+$(call soong_config_set,android_hardware_audio,run_64bit,true)
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl:64 \
