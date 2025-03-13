134830e0ebdd789209b454b1852440017a403c9b
Niyush-04 <mauryapradip04@gmail.com>
2025-03-13 06:06:08 +0530

    breeze: Drop NFC

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -268,12 +268,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
-ODM_MANIFEST_SKUS += hceese hcesim hcesim1 hcesim1ese hcesimese
-ODM_MANIFEST_HCEESE_FILES := $(DEVICE_PATH)/configs/vintf/manifest_hceese.xml
-ODM_MANIFEST_HCESIM_FILES := $(DEVICE_PATH)/configs/vintf/manifest_hcesim.xml
-ODM_MANIFEST_HCESIM1_FILES := $(DEVICE_PATH)/configs/vintf/manifest_hcesim1.xml
-ODM_MANIFEST_HCESIM1ESE_FILES := $(DEVICE_PATH)/configs/vintf/manifest_hcesim1ese.xml
-ODM_MANIFEST_HCESIMESE_FILES := $(DEVICE_PATH)/configs/vintf/manifest_hcesimese.xml
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
