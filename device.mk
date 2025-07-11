036e234791a72c01aceb47f9583d7813763830ca
Arian <arian.kulmer@web.de>
2025-07-11 16:04:09 +0200

    breeze: Don't explicitly build libwfdaac_vendor
    This has been converted to blueprint and is built implicitly as
    dependency of libwfdmmsrc_proprietary.so.
    
    Change-Id: I82bcca5f9aa85793c7b5a366e35472997e57feaf

--- device.mk
+++ device.mk
@@ -438,7 +438,3 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
-
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libwfdaac_vendor
