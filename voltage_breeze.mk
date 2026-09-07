6c404435f0b1faccd047f6276f4d25638b396d0b
Xen <xenthisside@gmail.com>
2026-09-08 02:10:45 +0530

    Update and rename lineage_breeze.mk to voltage_breeze.mk

--- voltage_breeze.mk
+++ voltage_breeze.mk
@@ -10,10 +10,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Configure full_base_telephony.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit common lineage configurations
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit common voltage configurations
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/lineage/config/BoardConfigReservedSize.mk)
+$(call inherit-product, vendor/voltage/config/BoardConfigReservedSize.mk)
 
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/breeze/device.mk)
@@ -23,7 +23,7 @@ $(call inherit-product, vendor/xiaomi/breeze/breeze-vendor.mk)
 
 ## Device identifier
 PRODUCT_DEVICE := breeze
-PRODUCT_NAME := lineage_breeze
+PRODUCT_NAME := voltage_breeze
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 24066PC95I
 PRODUCT_MANUFACTURER := Xiaomi
@@ -36,3 +36,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
